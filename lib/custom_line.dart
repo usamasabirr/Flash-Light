3f1dd3e58acd5a8db864968fb36d21310489b35f
Usama Sabir <usamasabir@192.168.1.2>
2023-03-26 16:54:34 +0500

    implemented 	1- using quadraticBezierTo draw lines that will appear once cord reached back to the original position

--- lib/custom_line.dart
+++ lib/custom_line.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
 
 class LinePainter extends CustomPainter {
-  Offset position;
-  LinePainter(this.position);
+  Offset startPosition;
+  Offset endPosition;
+  LinePainter(this.startPosition, this.endPosition);
   @override
   void paint(Canvas canvas, Size size) {
     print('size is $size');
     Paint paint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 10;
+      ..color = Colors.grey
+      ..strokeWidth = 4;
 
-    Offset startingPoint = Offset(50, 50);
-    Offset endingPoint = position;
+    Offset startingPoint = startPosition;
+    Offset endingPoint = endPosition;
 
     canvas.drawLine(startingPoint, endingPoint, paint);
   }
@@ -21,59 +22,70 @@ class LinePainter extends CustomPainter {
   bool shouldRepaint(LinePainter oldDelegate) => true;
 }
 
+class CurvePainter1 extends CustomPainter {
+  Offset startPosition;
+  Offset endPosition;
+  CurvePainter1(this.startPosition, this.endPosition);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.green;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = 4.0;
+
+    var path = Path();
+
+    path.moveTo(startPosition.dx + 10, startPosition.dy);
+    path.quadraticBezierTo(startPosition.dx + 15, startPosition.dy + 25,
+        startPosition.dx - 20, startPosition.dy + 30);
+    path.quadraticBezierTo(startPosition.dx - 20, startPosition.dy + 38,
+        startPosition.dx, startPosition.dy + 55);
+
+    //path.lineTo(size.width, 0);
+    //path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CurvePainter1 oldDelegate) => true;
+}
+
 class Change extends StatefulWidget {
   @override
   State<Change> createState() => _ChangeState();
 }
 
 class _ChangeState extends State<Change> with SingleTickerProviderStateMixin {
-  Offset position = Offset(50, 150);
+  final double x1 = 100.0;
+  final double y1 = 250.0;
+  final double x2 = 100.0;
+  final double y2 = 350.0;
+  late Offset startPosition;
+  late Offset endPosition;
 
   late Animation<Offset> _animation;
   late AnimationController _controller;
 
-  // void _runAnimation(Offset pixelsPerSecond, Size size) {
-  //   _animation = _controller.drive(
-  //     AlignmentTween(
-  //       begin: _dragAlignment,
-  //       end: Alignment.center,
-  //     ),
-  //   );
-  //   // Calculate the velocity relative to the unit interval, [0,1],
-  //   // used by the animation controller.
-  //   final unitsPerSecondX = pixelsPerSecond.dx / size.width;
-  //   final unitsPerSecondY = pixelsPerSecond.dy / size.height;
-  //   final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
-  //   final unitVelocity = unitsPerSecond.distance;
-
-  //   const spring = SpringDescription(
-  //     mass: 20,
-  //     stiffness: 0.1,
-  //     damping: 1,
-  //   );
-
-  //   final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
-
-  //   _controller.animateWith(simulation);
-  // }
-
   @override
   void initState() {
     super.initState();
+    startPosition = Offset(x1, y1);
+    endPosition = Offset(x2, y2);
     _controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 200));
 
     _controller.addListener(() {
       if (_controller.isCompleted) {}
       setState(() {
-        position = _animation.value;
+        endPosition = _animation.value;
       });
     });
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     _controller.dispose();
     super.dispose();
   }
@@ -81,31 +93,37 @@ class _ChangeState extends State<Change> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onPanDown: (details) {
-          print('down');
-        },
-        onPanUpdate: (details) {
-          setState(() {
-            position =
-                Offset(details.globalPosition.dx, details.globalPosition.dy);
-          });
-        },
-        onPanEnd: (details) {
-          print('this');
-          _animation = Tween<Offset>(
-            begin: position,
-            end: Offset(50, 150),
-          ).animate(
-              CurvedAnimation(parent: _controller, curve: Curves.bounceInOut));
+      body: Stack(children: [
+        GestureDetector(
+          onPanDown: (details) {
+            print('down');
+          },
+          onPanUpdate: (details) {
+            setState(() {
+              endPosition =
+                  Offset(details.globalPosition.dx, details.globalPosition.dy);
+            });
+          },
+          onPanEnd: (details) {
+            print('this');
+            _animation = Tween<Offset>(
+              begin: endPosition,
+              end: Offset(100, 350),
+            ).animate(CurvedAnimation(
+                parent: _controller, curve: Curves.elasticInOut));
 
-          _controller.forward();
-        },
-        child: CustomPaint(
-          child: Container(),
-          painter: LinePainter(position),
+            _controller.forward();
+          },
+          child: CustomPaint(
+            child: Container(),
+            painter: LinePainter(startPosition, endPosition),
+          ),
         ),
-      ),
+        CustomPaint(
+          child: Container(),
+          painter: CurvePainter1(startPosition, endPosition),
+        ),
+      ]),
     );
   }
 }
